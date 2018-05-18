896ef627e92803e7232d3ab9f1670635fae7e317
Li Wentai <liweta@sjtu.edu.cn>
2018-05-19 01:02:59 +0800

    visual

--- run.py
+++ run.py
@@ -3,15 +3,25 @@
 import visual
 import pagerank
 
+filename = 'input'
+
 if __name__ == "__main__":
     P=2
     Q=1
     cacheSize=1
     memorySize=1
 
-    pagerank=pagerank.PageRank(P,Q,'input')
+    inputfile = open(filename, "r")
+    line = inputfile.readline()
+    sizeV = map(int, line.split())[0]
+    inputfile.close()
+    GV = visual.GV(sizeV,cacheSize,memorySize,P,Q)
+
+    pagerank=pagerank.PageRank(P,Q,filename,GV)
     pagerank.preprocess()
 
-    GV=visual.GV(pagerank.getV(),cacheSize,memorySize,P,Q)
+    # pagerank.do_pagerank(GV)
+    # Change the 1st parameter of the following function to "next step". DO NOT delete lambda
+    GV.setButton(lambda: 1, lambda: pagerank.do_pagerank(GV))
 
-    pagerank.do_pagerank(GV)
+    GV.draw()
